20a616aa310d9abf2c6a2e5c68a38805d16e6714
OnyekachiMiracleNechi <168313634+OnyekachiMiracleNechi@users.noreply.github.com>
2024-09-29 12:34:12 +0100

    Prep 3 update

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -10,7 +10,7 @@ class Program
 
     static void PlayGuessingGame()
     {
-        // Time to generate a magic number for you to guess!
+        // Generating the magic number for you to guess!
         Random randomGenerator = new Random();
         int magicNumber = randomGenerator.Next(1, 101);
         int guess = -1;
@@ -19,20 +19,20 @@ class Program
         Console.WriteLine("Welcome to the Magic Number Guessing Game!");
         Console.WriteLine("Your goal is to guess the magic number between 1 and 100.");
 
-        // Keep guessing until you find the magic number
+        
         while (guess != magicNumber)
         {
             Console.Write("Take a shot! What’s your guess? ");
-            // Let’s make sure you enter a valid number
+            // Making sure the user enters a valid number
             if (!int.TryParse(Console.ReadLine(), out guess) || guess < 1 || guess > 100)
             {
                 Console.WriteLine("Oops! Please enter a number between 1 and 100.");
-                continue; // If the input isn't valid, try again
+                continue; 
             }
 
             numberOfGuesses++;
 
-            // Give hints based on your guess
+            // Giving the user a hint
             if (magicNumber > guess)
             {
                 Console.WriteLine("Higher! You’re getting closer!");
@@ -48,13 +48,14 @@ class Program
             }
         }
 
-        // Let’s see if you want to try again
+        // Prompt for continuation
         Console.WriteLine("Want to play again? (yes/no)");
         string playAgain = Console.ReadLine().ToLower();
 
         if (playAgain == "yes")
         {
-            PlayGuessingGame(); // Restart the fun
+        
+            PlayGuessingGame(); 
         }
         else
         {
