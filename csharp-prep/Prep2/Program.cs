cfce4f1cf046b6d0c52f2a8c01b50f2af0a7f7b3
OnyekachiMiracleNechi <168313634+OnyekachiMiracleNechi@users.noreply.github.com>
2024-09-29 11:11:44 +0100

    Prep 2 Assignment

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        //Pronpt for the user's percentage
+        Console.WriteLine("What is your grade percentage? ");
+         string answer = Console.ReadLine();
+        int percent = int.Parse(answer);
+
+        string remark = "";
+
+        if (percent >= 90)
+        {
+            remark = "A";
+        }
+        else if (percent >= 80)
+        {
+            remark = "B";
+        }
+        else if (percent >= 70)
+        {
+           remark = "C";
+        }
+        else if (percent >= 60)
+        {
+            remark = "D";
+        }
+        else
+        {
+            remark = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {remark}");
+        
+        if (percent >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time!");
+        }
     }
 }
